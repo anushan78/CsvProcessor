466c1f820aa90079fb4e9b05c959b8d3ef9cd52a
Anushan Tennakoon <anushan.tennakoon@rxpservices.com>
2019-05-22 22:22:18 +1000

    Created test project

---
+++ CsvProcessor.Test/CsvFileServiceTests.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using Moq;
+using CsvProcessor.Interfaces;
+
+namespace CsvProcessor.Test
+{
+    public class CsvFileServiceTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
